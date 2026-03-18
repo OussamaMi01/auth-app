6541f5c0c01f66081bfefa8f1c1fe77af0dcb8f8
OussamaMi01 <missaouioussama774@gmail.com>
2026-03-18 16:35:23 +0100

    fixing static page rendering

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -3,9 +3,7 @@
 'use client';
 
 export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-export const fetchCache = 'force-no-store';
-export const runtime = 'nodejs';
+
 
 
 import { Suspense, useState } from "react";
